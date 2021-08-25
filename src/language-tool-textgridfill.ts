0baad9ade699cb6a540bc9c3199c240c10a43ee6
Leeway <leewayzhang@outlook.com>
2021-08-26 00:38:45 +0800

    update

--- src/language-tool-textgridfill.ts
+++ src/language-tool-textgridfill.ts
@@ -6,12 +6,16 @@ import { serializeTextgrid } from './lib/praatio/textgrid_io';
 
 commander.usage('<textgrid file path>')
 	.option('--source <source txt>', '源文本')
+	.option('--from <from id>', '从哪个id开始')
+	.option('--to <to id>', '从哪个id结束')
 	.option('--skip-first', '是否跳过首段');
 commander.parse(process.argv);
 
 const textgridPath = commander.args[0];
 
 const source = commander.source;
+const from = parseInt(commander.from);
+const to = parseInt(commander.to);
 const skipFirst = commander.skipFirst;
 
 (async () => {
@@ -20,11 +24,21 @@ const skipFirst = commander.skipFirst;
 	if (skipFirst) {
 		textgridProcessor.writeLine('');
 	}
+	let start = false;
 	for await (const line of txt.getLines()) {
 		const splits = line.split(/ |\t/);
 		const value = splits[splits.length - 1].trim();
-		textgridProcessor.writeLine(value);
-		textgridProcessor.writeLine('');
+		const id = parseInt(splits[0]);
+		if (id === from) {
+			start = true;
+		}
+		if (start) {
+			textgridProcessor.writeLine(value);
+			textgridProcessor.writeLine('');
+		}
+		if (id === to) {
+			start = false;
+		}
 	}
 	const res = serializeTextgrid(textgridProcessor.textgrid);
 	writeFileSync(textgridProcessor.fileInfo.dir + '/' + textgridProcessor.filename + '_new' + textgridProcessor.fileInfo.ext, res, { encoding: 'utf-8' });
